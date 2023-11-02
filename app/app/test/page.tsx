7578961ba84032f5917bb75698f4b42b50c1c752
Kerimcan <464639@student.fontys.nl>
2023-11-02 21:13:56 +0100

    feat: antd app router setup

--- app/app/test/page.tsx
+++ app/app/test/page.tsx
@@ -1,6 +1,7 @@
 "use client"; // If used in Pages Router, is no need to add "use client"
 
 import React from "react";
+import { Button } from "antd";
 
 const Home = () => (
   <div className="App">
