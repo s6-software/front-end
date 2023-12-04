afd33ab928db567a8a65bf06ff05e761d08d9b61
Kerimcan Güney <464639@student.fontys.nl>
2023-12-04 21:18:14 +0100

    fix: cards

--- my-app/src/app/(Protected)/dashboard/page.tsx
+++ my-app/src/app/(Protected)/dashboard/page.tsx
@@ -10,7 +10,7 @@ const page = () => {
               Dashboard
             </p>
           </div>
-
+          <h1>Header title</h1>
           <WorkspaceExplorer />
         </div>
       </div>
@@ -19,9 +19,54 @@ const page = () => {
 };
 
 const WorkspaceExplorer = () => {
+  const workspaces = [
+    "card 1",
+    "card 2",
+    "card 3",
+    "card 4",
+    "card 5",
+    "card 6",
+  ];
   return (
-    <div>
-      <div>hel</div>
+    <div className="flex overflow-x-scroll overflow-y-hidden w-full">
+      {workspaces.map((workspace, index) => (
+        <WorkspaceCardItem
+          key={index}
+          name={workspace}
+          description={`Description for ${workspace}`}
+          id={`workspace-${index}`}
+        />
+      ))}
+    </div>
+  );
+};
+
+interface WorkspaceCardItemProps {
+  name: string;
+  description: string;
+  id: string;
+}
+
+const WorkspaceCardItem = ({
+  name,
+  description,
+  id,
+}: WorkspaceCardItemProps) => {
+  return (
+    <div className="flex">
+      <a
+        href="#"
+        className="text-center w-60 h-40 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      >
+        <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {name}
+        </h1>
+        <div className="flex justify-center h-full align-middle">
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            {description}
+          </p>
+        </div>
+      </a>
     </div>
   );
 };
