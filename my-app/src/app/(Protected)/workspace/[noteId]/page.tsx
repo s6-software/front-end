f8de45e1b15899dbbf3d0cb31a6ed1bfb62c3cab
Kerimcan Güney <464639@student.fontys.nl>
2023-12-07 14:09:17 +0100

    Update page layout in workspace

--- my-app/src/app/(Protected)/workspace/[noteId]/page.tsx
+++ my-app/src/app/(Protected)/workspace/[noteId]/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
 
-import EditorJS, { OutputData } from "@editorjs/editorjs";
-import { EDITOR_JS_TOOLS } from "./tools";
 export default function Home({ params }: any) {
   return (
     <div className="bg-white flex w-full h-view">
       <div className="flex bg-gray-100 w-3/5 mx-auto justify-center">
-        <div className="w-full h-screen my-auto overflow-auto">
+        <div className="w-full h-screen my-auto overflow-y-auto overflow-x-hidden ml-auto">
           <NewEditor />
         </div>
       </div>
