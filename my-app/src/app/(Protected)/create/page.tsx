8fd3b7256e700f3e970c7ee5e119c815f4955b6a
Kerimcan <464639@student.fontys.nl>
2023-12-25 23:03:21 +0100

    create workspace validation

--- my-app/src/app/(Protected)/create/page.tsx
+++ my-app/src/app/(Protected)/create/page.tsx
@@ -1,14 +1,19 @@
 "use client";
 import React from "react";
 import { CheckIcon } from "@heroicons/react/20/solid";
-import { CheckBadgeIcon, XCircleIcon } from "@heroicons/react/24/outline";
+import { XCircleIcon } from "@heroicons/react/24/outline";
 const page = () => {
   const [isValid, setIsValid] = React.useState(false);
+  const [submitted, setSubmitted] = React.useState(false);
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setSubmitted(true);
+  };
   return (
     <div className=" w-full h-screen flex">
       <div className="flex bg-gray-100 border-gray-300  border w-3/4 h-screen mx-auto justify-center">
-        <form className="flex flex-col w-full justify-center">
+        <form className="flex flex-col w-full justify-center"  onSubmit={handleSubmit}>
           <div className="flex text-3xl justify-center mt-12">
             <h1 className="text">Create a new workspace</h1>
           </div>
@@ -18,16 +23,27 @@ const page = () => {
           <div className="flex justify-center h-1/3">
             <textarea
               className="w-1/2 h-1/2 border-2 border-gray-300 p-2 rounded-lg m-2"
-              placeholder="Workspace description"
+              placeholder="description (optional)"
               style={{ resize: "none" }}
             />
           </div>
           <div className="flex justify-center">
             <button
-              className="bg-blue-500 text-white px-6 py-2 rounded-md m-2"
-              disabled={!isValid}
+              className={`bg-${isValid ? 'blue' : 'gray'}-500 text-white px-6 py-2 rounded-md m-2`}
+              disabled={!isValid || submitted}
+              type="submit"
             >
-              Create
+              {submitted ? (
+                <div className="flex flex-row">
+                  <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                  </svg>
+                  <p>loading...</p>
+                </div>
+              ) : (
+                'Create'
+              )}
             </button>
           </div>
         </form>
