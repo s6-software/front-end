79718b2a1b37b0fa81f2221fa3da649d102a358a
Kerimcan <464639@student.fontys.nl>
2023-12-25 22:14:11 +0100

    removed comments

--- my-app/src/app/api/auth/[...nextauth]/route.ts
+++ my-app/src/app/api/auth/[...nextauth]/route.ts
@@ -4,15 +4,9 @@ import CredentialsProvider from "next-auth/providers/credentials";
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const authOptions = {
-  // Configure one or more authentication providers
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
       name: "Credentials",
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
         username: {
           label: "Username",
@@ -52,4 +46,4 @@ const authOptions = {
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export {  handler as POST };
