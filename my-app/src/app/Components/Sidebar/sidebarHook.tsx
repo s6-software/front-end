8d0cfae123416da50aa00ba0cdc88b980dca2f64
Kerimcan <464639@student.fontys.nl>
2024-01-03 23:27:09 +0100

    context hook for sidebar folder

--- my-app/src/app/Components/Sidebar/sidebarHook.tsx
+++ my-app/src/app/Components/Sidebar/sidebarHook.tsx
@@ -33,61 +33,98 @@ export const useCurrentWorkspace = () => {
   return [currentWorkspace, setCurrentWorkspace];
 };
 
-export const WorkspaceInstance = ({}) => {
-  const [currentWorkspace, setCurrentWorkspace] = useState("");
-  const [allWorkspaces, setAllWorkspaces] = useState([{}]);
-  const [currentSelectedNote, setCurrentSelectedNote] = useState();
-  const [allNotes, setAllNotes] = useState();
+// export const WorkspaceInstance = ({}) => {
+//   const [currentWorkspace, setCurrentWorkspace] = useState("");
+//   const [allWorkspaces, setAllWorkspaces] = useState([{}]);
+//   const [currentSelectedNote, setCurrentSelectedNote] = useState();
+//   const [allNotes, setAllNotes] = useState();
 
-  useEffect(() => {}, []);
+//   useEffect(() => {}, []);
 
-  const initWorkspace = (credentials: string) => {
-    // send request to server
-    const link = "https://jsonplaceholder.typicode.com/todos/1";
-    console.log(credentials);
-    fetch(link)
-      .then((response) => response.json())
-      .then((json) => console.log(json));
-    // set all workspaces
+//   const initWorkspace = (credentials: string) => {
+//     // send request to server
+//     const link = "https://jsonplaceholder.typicode.com/todos/1";
+//     console.log(credentials);
+//     fetch(link)
+//       .then((response) => response.json())
+//       .then((json) => console.log(json));
+//     // set all workspaces
 
-    setAllWorkspaces([
-      {
-        name: "Research notes",
-        description: "This is the max amount of chars",
-        id: "1",
-        shared: true,
-        owner: true,
-        time: "2 days ago",
-      },
-      {
-        name: "Research notes",
-        description: "This is the max amount of chars",
-        id: "2",
-        shared: false,
-        owner: false,
-        time: "1 days ago",
-      },
-      {
-        name: "Research notes",
-        description: "This is the max amount of chars",
-        id: "3",
-        shared: false,
-        owner: false,
-        time: "3 days ago",
-      },
-    ]);
-  };
+//     setAllWorkspaces([
+//       {
+//         name: "Research notes",
+//         description: "This is the max amount of chars",
+//         id: "1",
+//         shared: true,
+//         owner: true,
+//         time: "2 days ago",
+//       },
+//       {
+//         name: "Research notes",
+//         description: "This is the max amount of chars",
+//         id: "2",
+//         shared: false,
+//         owner: false,
+//         time: "1 days ago",
+//       },
+//       {
+//         name: "Research notes",
+//         description: "This is the max amount of chars",
+//         id: "3",
+//         shared: false,
+//         owner: false,
+//         time: "3 days ago",
+//       },
+//     ]);
+//   };
 
-  // return <WorkspaceContext.Provider value={}}></WorkspaceContext.Provider>;
+// return <WorkspaceContext.Provider value={}}></WorkspaceContext.Provider>;
+// };
+
+// export const useWorkspaceInstance = () => {
+//   const context = useContext(WorkspaceContext);
+
+//   if (!context) {
+//     throw new Error(
+//       "useWorkspaceInstance must be used within a WorkspaceInstance"
+//     );
+//   }
+
+//   return context;
+// };
+import React from "react";
+type FoldersContextType = [any[], React.Dispatch<React.SetStateAction<any[]>>];
+
+const FoldersContext = createContext<FoldersContextType | undefined>(undefined);
+
+export const FoldersProvider = ({ children }) => {
+  const [folders, setFolders] = useState<any[]>([]);
+
+  useEffect(() => {
+    const storedFolders = localStorage.getItem("Folders");
+    if (storedFolders) {
+      setFolders(JSON.parse(storedFolders));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (folders.length > 0) {
+      localStorage.setItem("Folders", JSON.stringify(folders));
+    }
+  }, [folders]);
+
+  return (
+    <FoldersContext.Provider value={[folders, setFolders]}>
+      {children}
+    </FoldersContext.Provider>
+  );
 };
 
-export const useWorkspaceInstance = () => {
-  const context = useContext(WorkspaceContext);
+export const useFolders = () => {
+  const context = useContext(FoldersContext);
 
   if (!context) {
-    throw new Error(
-      "useWorkspaceInstance must be used within a WorkspaceInstance"
-    );
+    throw new Error("useFolders must be used within a FoldersProvider");
   }
 
   return context;
